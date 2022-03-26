9dab28fe13e55f26e5035f38c57f1627547a53c6
Atthapon Srikosak <dr.till007@gmail.com>
2022-03-27 05:26:24 +0700

    Return Version

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -4,25 +4,13 @@ const UserServices = {
   userLogin(body) {
     return requests.post('/user/login', body);
   },
-  coinposUserLogin(body) {
-    return requests.post('/user/coinPOS-login', body);
-  },
 
   verifyEmailAddress(body) {
     return requests.post('/user/verify-email', body);
   },
-  verifyCoinPOSEmailAddress(body) {
-    return requests.post('/user/verify-coinpos-email', body);
-  },
 
   userRegister(token, body) {
     return requests.post(`/user/register/${token}`, body);
-  },
-  coinposUserRegister(token, body) {
-    return requests.post(`/user/coinpos-register/${token}`, body);
-  },
-  coinposCheckExpired(body) {
-    return requests.post(`/user/coinpos-check-expired`, body);
   },
 
   signUpWithProvider(body) {
@@ -32,15 +20,9 @@ const UserServices = {
   forgetPassword(body) {
     return requests.put('/user/forget-password', body);
   },
-  forgetCoinPOSCustomerPassword(body) {
-    return requests.put('/user/coinpos-customer-forget-password', body);
-  },
 
   resetPassword(body) {
     return requests.put('/user/reset-password', body);
-  },
-  resetCoinPOSCustomerPassword(body) {
-    return requests.put('/user/coinpos-customer-reset-password', body);
   },
 
   changePassword(body) {
@@ -50,10 +32,6 @@ const UserServices = {
   updateUser(id, body) {
     return requests.put(`/user/${id}`, body);
   },
-  getLiffURLTemplate()
-  {
-    return requests.get(`/user/allUser1`);
-  }
 };
 
 export default UserServices;
