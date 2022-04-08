c75405704b7740e2b8007cf5f0e38c9cdc9a5335
Atthapon Srikosak <dr.till007@gmail.com>
2022-04-08 16:09:38 +0700

    Change LiffTemplate Service

--- src/services/ProductServices.js
+++ src/services/ProductServices.js
@@ -527,6 +527,43 @@ const ProductServices = {
     }
     
   },
+  async fetchGetLiffURLTemplate()
+  {
+    try
+    {
+      //res.send("getLiff URL");
+      //return;
+      //var template = "";
+      var url = serviceUrl + 'GetLiffURLTemplate'
+      //res.send("getLiff URL = " + url);
+      //return;
+      var template = null;
+      console.log("getLiff URL")
+      await fetch(url,
+        { 
+          method:'GET',
+          //credentials:"include",
+          headers: {'Content-Type': 'application/json','x-security-lock':'0241CCFF2D40AF7AF8A4FC02272C47A30D15DBDFB36E3266D1296212574F328E'},
+          
+        }).then(function(response) {
+          return response.text();
+        }).then(function(data) {
+
+          console.log(data);
+          var obj = data;
+          template = obj;
+        });
+      
+      
+
+        return template;
+    }
+    catch(err) {
+      return "Error: " + err.message;
+      
+    }
+  }
+};
 
 };
 
